e38751437cbce58e6aa4af24f3f50cb443dd9384
Pawel Majchrzak <pawel.majchrzak.programista@gmail.com>
2023-01-09 21:38:29 +0100

    add recaptcha in registration

--- registration.php
+++ registration.php
@@ -1,3 +1,15 @@
+<?php
+
+    session_start();
+
+	if (isset($_POST['email']))
+	{
+
+		
+	}
+
+?>
+
 <!DOCTYPE HTML>
 <html lang="pl">
 <head>
@@ -16,6 +28,9 @@
 	<link href="https://fonts.googleapis.com/css2?family=Caveat&family=Lato:wght@300;400;700&family=Poppins:wght@200;400&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="css/fontello.css" type="text/css" />
 	<link rel="icon" href="image/logo.png" type="image/png">
+
+	<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
 </head>
 
 <body>
@@ -34,30 +49,42 @@
 				Rejestracja
 			</header>
 			
-			<form>
+			<form method="post">
 
 				<div class="input-group p-3 m-auto">
 					<span class="input-group-text w-25">Imię</span>
-					<input type="text" class="form-control" placeholder="Podaj imię" aria-label="Name" required>
+					<input type="text" class="form-control" placeholder="Podaj imię" aria-label="Name" name="username" required>
 				</div>
 
 				<div class="input-group p-3 m-auto">
 					<span class="input-group-text w-25">E-mail</span>
-					<input type="email" class="form-control" placeholder="Podaj adres Email" aria-label="Email" required>
+					<input type="email" class="form-control" placeholder="Podaj adres Email" aria-label="Email" name="email" required>
 				</div>
 
 				<div class="input-group p-3 m-auto">
 					<span class="input-group-text w-25">Hasło</span>
-					<input type="password" class="form-control" placeholder="Podaj hasło" aria-label="Password" required>
+					<input type="password" class="form-control" placeholder="Podaj hasło" aria-label="Password" name="password" required>
 				</div>
 				
 				<div class="col-12 col-md-11 col-lg-9 m-auto">
 					<div class="ms-3 p-3 fs-6">
-						<input type="checkbox" class="form-check-input" value="a" name="wyp[]" required> Akceptuję <a href="" target="_blank" class="link">Regulamin</a> i <a href="" target="_blank" class="link">Politykę prywatności</a>
+						<label>
+							<input type="checkbox" class="form-check-input" value="a" name="statute" required> Akceptuję <a href="" target="_blank" class="link">Regulamin</a> i <a href="" target="_blank" class="link">Politykę prywatności</a>
+						</label>
 						<br />
 						Masz już konto? <a href="login.php" class="link">Zaloguj się</a>
 					</div>
+					
+					<div class="position-absolute start-50 translate-middle-x">
+						<div class="g-recaptcha" data-sitekey="6LeQ1OQjAAAAADZ_Iswn1RcAXpgAWXbxjNq0Go0n"></div>
+						
+					</div>
+					<br /><br />
 				</div>
+
+				
+
+				
 				
 				<div class="btn-group btn-group-lg mt-5 start-50 translate-middle">
 					<button type="submit" class="btn btn-success">Zarejestruj się</button>
@@ -68,9 +95,11 @@
 		</div>
 
 	</article>
+
+
 	
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-	<script src="js/bootstrap.min.js"></script>	
+	<script src="js/bootstrap.min.js"></script>
 
 </body>
 </html>
